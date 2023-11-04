54d5818e7609d28f7bbc16e32b094d0d53d08ba2
Sam <sam@squashedpixel.co.uk>
2023-11-04 11:00:17 +0000

    removed unused yarn files. Updated eslint to support standard rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,11 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/standard'
+    // 'eslint:recommended', // use @vue/standard instead
+    // '@vue/eslint-config-prettier/skip-formatting' // using @vue/standard instead
   ],
   parserOptions: {
     ecmaVersion: 'latest'
