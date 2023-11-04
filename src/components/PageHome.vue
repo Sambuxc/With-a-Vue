96b2c0c4a2a84e534b79fc415e17209bd69a15f2
Sam <sam@squashedpixel.co.uk>
2023-11-04 14:40:06 +0000

    Styled the post lists area and app from row to column

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,10 +1,12 @@
 <template>
   <div v-for="thread in threads" :key="thread.id">
-    <h2>{{ thread.title }}</h2>
+    <h2 class="thread-title">{{ thread.title }}</h2>
 
-    <div class="post" v-for="post in posts" :key="post.id">
-      <p>{{ postById(post.id).text }}</p>
-      <h3>Written by {{ userById(postById(post.id).userId).name }}</h3>
+    <div class="post-list">
+      <div class="post" v-for="post in posts" :key="post.id">
+        <p class="post-content">{{ postById(post.id).text }}</p>
+        <h3 class="post-author">Written by {{ userById(postById(post.id).userId).name }}</h3>
+      </div>
     </div>
   </div>
 </template>
@@ -26,7 +28,43 @@ function userById (userId) {
 </script>
 
 <style lang="scss" scoped>
+.thread-title {
+  margin-top: 18px;
+  margin-bottom: 9px;
+  font-weight: 700;
+  font-size: 3rem;
+}
+
+.post-list {
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-template-rows: masonry;
+
 .post {
-  margin-bottom: 15px;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: rgba(255,255,255,0.1);
+  border-radius: 9px;
+
+  &-author {
+    font-weight: 600;
+    font-size: 12px;
+  }
+
+  &-content {
+    margin-bottom: 5px;
+    max-height: 200px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 14px;
+
+    &:hover {
+      overflow-y: scroll;
+    }
+  }
+}
 }
 </style>
